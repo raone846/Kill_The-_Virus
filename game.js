055ca3fc280a8b7f39f3620dcbc4ba3e667d30cb
raone846 <rohanpandey846@gmail.com>
2020-06-04 15:24:20 +0530

    added player and visus

--- game.js
+++ game.js
@@ -1,6 +1,13 @@
 function load_images(){
     //player,gem and virus
+    enemy_image = new Image;
+    enemy_image.src = "Assets/v1.png";
     
+    player_img = new Image;
+    player_img.src = "Assets/superhero.png";
+    
+    gem_img = new Image;
+    gem_img.src = "Assets/gemm.png";
 }
 
 
@@ -17,15 +24,71 @@ function init(){
     // create a context
     pen = canvas.getContext('2d');
     console.log(pen);
+    game_over = false;
     
-    box = {
+    e1 = {
         x : 150,
         y : 50,
         w : 60,
         h : 60,
-        speed : 5
+        speed : 20
     };
     
+    e2 = {
+        x : 300,
+        y : 150,
+        w : 60,
+        h : 60,
+        speed : 30
+    };
+    
+    e3 = {
+        x : 450,
+        y : 20,
+        w : 60,
+        h : 60,
+        speed : 40,
+    };
+    
+    enemy = [e1,e2,e3];
+    
+    player = {
+        x : 20,
+        y : H/2,
+        w : 60,
+        h : 60,
+        speed : 20,
+        moving : false,
+        health : 100,
+    };
+    
+    gem = {
+        x : W-100,
+        y : H/2,
+        w : 60,
+        h : 60
+    };
+    //listen events on canvas
+    canvas.addEventListener('mousedown',function(){
+        console.log("Mouse pressed");
+        player.moving = true; 
+    });
+    canvas.addEventListener('mouseup',function(){
+        console.log("Mouse released");
+        player.moving = false; 
+    });
+    
+    
+}
+
+function isOverlap(rect1,rect2){
+    if(rect1.x < rect2.x + rect2.w &&
+      rect1.x + rect1.w > rect2.x && 
+      rect1.y < rect2.y + rect2.h &&
+      rect1.y + rect1.h > rect2.y){
+        return true;
+    }
+    return false;
 }
 
 function draw(){
@@ -34,22 +97,62 @@ function draw(){
     pen.clearRect(0,0,W,H);
     
     pen.fillStyle = "red";
-    pen.fillRect(box.h,box.y,box.w,box.h);
+    //pen.fillRect(box.h,box.y,box.w,box.h);
+    //pen.drawImage(enemy_image,box.h,box.y,box.w,box.h);
     
+    //draw the player and gem
+    pen.drawImage(player_img,player.x,player.y,player.w,player.h);
+    pen.drawImage(gem_img,gem.x,gem.y,gem.w,gem.h);
+    
+    
+    for(let i=0;i<enemy.length;i++){
+        pen.drawImage(enemy_image,enemy[i].x,enemy[i].y,enemy[i].w,enemy[i].h);
+    }
+    
+    pen.fillStyle = "white";
+    pen.fillText("Score"+player.health,10,10);
 }
 
 function update(){
     
-    //move the box
-    box.y += box.speed;
+    //check player movement
+    if(player.moving==true){
+        player.x += player.speed;
+        player.health += 20;
+    }
     
-    if(box.y>= H - box.h || box.y < 0){
-        box.speed *= -1;
+    for(let i=0;i<enemy.length;i++){
+        if(isOverlap(enemy[i],player)){
+            player.health -= 50;
+            if(player.health <0){
+                console.log(player.health);
+                game_over = true;
+                alert("Game Over" + player.health);
+            }
+        }
+    }
+    //overlap
+    if(isOverlap(player,gem)){
+        console.log("you win");
+        alert("You won!!!");
+        game_over = true;
+        return;
+    }
+    
+    //move the box
+   //update enemy
+    for(let i=0;i<enemy.length;i++){
+        enemy[i].y += enemy[i].speed;
+        if(enemy[i].y>H-enemy[i].h || enemy[i].y<0){
+            enemy[i].speed *= -1;
+        }
     }
     
 }
 
 function gameloop(){
+    if(game_over==true)
+        clearInterval(f);
     draw();
     update();
     console.log("In gameloop");
@@ -58,5 +161,3 @@ function gameloop(){
 load_images();
 init();
 var f = setInterval(gameloop,100);
-
-
